3367c594812dbc7d712efca843fa07f060461c27
Vi-Lain <vilain.alcr@gmail.com>
2026-08-05 16:43:10 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vi-Lain Official Website
+# Vi-Lain Official Website v5
 
 ## v5の主な変更
 - デザインを調整
--- data/live.js
+++ data/live.js
@@ -1,4 +1,4 @@
 window.LIVE_DATA = {
-  "updatedAt": "2026-08-05T07:30:11.202545+00:00",
-  "streams": []
+  updatedAt: null,
+  streams: []
 };
